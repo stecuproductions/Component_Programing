7294f7d8a0ec693ddb9b90d21f282ac5d3d25184
Julia <250353@edu.p.lodz.pl>
2025-03-14 14:00:12 +0100

    task1

---
+++ project1/src/main/java/comprog/project1/MessageFormatter.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package comprog.project1;
+
+/**
+ *
+ * @author jroga
+ */
+public class MessageFormatter {
+    public String format(String message) {
+        return "hello " + message + "!";
+    }
+}
