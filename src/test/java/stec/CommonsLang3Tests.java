f253824891d2abd3c6b0f3718635631c8c37f2bb
Julia <250353@edu.p.lodz.pl>
2025-04-16 08:36:34 +0200

    hashcode i tostring test

--- src/test/java/stec/CommonsLang3Tests.java
+++ src/test/java/stec/CommonsLang3Tests.java
@@ -1,5 +1,8 @@
 package stec;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +25,31 @@ public class CommonsLang3Tests {
             SudokuField[][] testBoard2Fields=SudokuTestsUtils.getPrivateBoardField(testBoard2);
             SudokuField[][] boardFields=SudokuTestsUtils.getPrivateBoardField(board);
             assertFalse(testBoard2Fields[0][0].equals(boardFields) );
+        }
+        
+        @Test
+        public void hashCodeTest() {
+            SudokuBoard testBoard = new SudokuBoard(solver);
+            assertFalse(board.equals(testBoard));
+            assertNotEquals(board.hashCode(), testBoard.hashCode());
+            for (int row = 0; row < 9; row++) {
+                for (int col = 0; col < 9; col++) {
+                    int val = board.get(row, col);
+                    testBoard.set(row, col, val);
+                }
+            }
+            //assertTrue(board.equals(testBoard));
+            assertEquals(board.hashCode(), testBoard.hashCode());
+        }
+        
+        @Test
+        public void toStringTest() {
+            String output = board.toString();
+            assertNotNull(output);
+            assertFalse(output.isBlank());
+            assertTrue(output.contains("SudokuBoard"));
+            assertTrue(output.contains("rows"));
+            assertTrue(output.contains("columns"));
+            assertTrue(output.contains("boxes"));
         }
 }
