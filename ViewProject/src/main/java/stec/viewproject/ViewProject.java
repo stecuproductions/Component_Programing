3b7f23adee0fc7876c2b0ad6822370020648994b
Julia <250353@edu.p.lodz.pl>
2025-04-28 22:58:56 +0200

    multimodule

---
+++ ViewProject/src/main/java/stec/viewproject/ViewProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package stec.viewproject;
+
+/**
+ *
+ * @author jroga
+ */
+public class ViewProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
