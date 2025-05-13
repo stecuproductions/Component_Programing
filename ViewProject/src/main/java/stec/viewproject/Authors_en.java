218a3ad6e366ca62874f9529efa859544dc8b79b
Julia <250353@edu.p.lodz.pl>
2025-05-13 10:57:16 +0200

    authors

---
+++ ViewProject/src/main/java/stec/viewproject/Authors_en.java
@@ -0,0 +1,25 @@
+package stec.viewproject;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import java.util.ListResourceBundle;
+
+/**
+ *
+ * @author jroga
+ */
+public class Authors_en extends ListResourceBundle {
+
+    @Override
+    protected Object[][] getContents() {
+        return new Object[][] {
+            {"authors", "Authors: Jakub Stec, Julia Rogalska"},
+            {"about.title", "About"}
+        };
+    }
+    
+}
